205cf7f0d53eb43c9fe26f6dec4caf7114ca910e
Matt Outen <mattoutenphysed@gmail.com>
2018-07-05 07:26:43 -0500

    added radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -60,6 +61,10 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
+      ':hover' : {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+      }
     };
 
     let persons = null;
@@ -80,6 +85,10 @@ class App extends Component {
       );
 
       style.backgroundColor = "red";
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     let classes = [];
@@ -103,5 +112,5 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+//higher order component
+export default Radium(App);
