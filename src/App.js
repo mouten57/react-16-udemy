898961a9c1077d56bc6ce85eef2779d2eaba7f3a
Matt Outen <mattoutenphysed@gmail.com>
2018-07-05 07:47:23 -0500

    remove radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -61,10 +60,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover' : {
-        backgroundColor: 'lightgreen',
-        color: 'black',
-      }
     };
 
     let persons = null;
@@ -85,10 +80,6 @@ class App extends Component {
       );
 
       style.backgroundColor = "red";
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
     }
 
     let classes = [];
@@ -100,7 +91,7 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
+
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           {/* this refers to the class */}
@@ -110,9 +101,8 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Switch Name</button>
           { persons }
         </div>
-      </StyleRoot>
+ 
     );
   }
 }
-//higher order component
-export default Radium(App);
+export default App;
